7e4a9120496dbd117556389873e8e42c5ca1e514
djangra2003 <dhruv81jangra@gmail.com>
2024-09-27 10:45:06 +0530

    POrtfolio

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+// import logo from "../assets/kevinRushLogo.png"
+import resume from "../assets/DHRUV_JANGRA_RESUME.pdf"
+import { FaLinkedin , FaGithub , FaFile} from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+const Navbar = () => {
+return <nav className="border-b mb-20 flex items-center justify-between py-6">
+    <div className=" flex flex-shrink-0 items-center">
+        <h1 className="ml-8 b-16 text-6xl font-bold  tracking-tight lg:mt-16 
+                    lg:text-3xl">DJ</h1>
+    </div>
+    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/dhruv-jangra-7315121a6/"><FaLinkedin /></a>
+        <a href="https://github.com/djangra2003"><FaGithub /></a>
+        <a href="https://leetcode.com/u/dhruvj_2003/"><SiLeetcode /></a>
+        <a href={resume}><FaFile/></a>
+    </div>
+</nav>
+}
+
+export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,99 @@
+import project1 from "../assets/projects/project-1.jpeg";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.jpg";
+// import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = ` I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like ReactJs, as well as back-end technologies like Node.js, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am currently pursuing B.Tech from Dronacharya Group of Institutions in CSE branch. I had completed my internship as a frontend intern from Dew Solutions. I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Node.js, MySQL,and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EDUCATION = [
+  {
+    Course:"BTech",
+    College:"Dronacharya Group of Instituions",
+    Year_of_Passing:"2025"
+    // Percentage:"70%"
+  },
+  {
+    Class:"12th",
+    School:"Faith Academy",
+    Year_of_Passing:"2021"
+    // Percentage:"70%"
+  },
+  {
+    Class:"10th",
+    School:"Faith Academy",
+    Year_of_Passing:"2019"
+    // Percentage:"70%"
+  }
+]
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Software Engineer Intern",
+    company: "Dew Solutions.",
+    description: `1) Contributed to developing an interactive website frontend using ReactJS and NodeJS, which leverages machine 
+                    learning algorithms to convert contracts and correspondences into text for easier usability. 
+                    /n 2)Containerized the application using Docker to enhance storage and processing efficiency, significantly reducing 
+time and resource consumption. `,
+    technologies: ["Javascript", "React.js", "Python","Docker"],
+  },
+  // {
+  //   year: "2022 - 2023",
+  //   role: "Frontend Developer",
+  //   company: "Adobe",
+  //   description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+  //   technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  // },
+  // {
+  //   year: "2021 - 2022",
+  //   role: "Full Stack Developer",
+  //   company: "Facebook",
+  //   description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+  //   technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  // },
+  // {
+  //   year: "2020 - 2021",
+  //   role: "Software Engineer",
+  //   company: "Paypal",
+  //   description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+  //   technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  // },
+];
+
+export const PROJECTS = [
+  {
+    title: "EDMS",
+    image: project1,
+    description:
+      "Contributed in internship to developing an interactive website frontend using ReactJS and NodeJS, which leverages machine learning algorithms to convert contracts and correspondences into text for easier usability. ",
+    technologies: ["Docker", "CSS", "React", "Node.js"],
+  },
+  {
+    title: "Jarvis ",
+    image: project3,
+    description:
+      "Applied my Python knowledge to develop a computer assistant capable of handling basic tasks such as capturing screenshots, activating the camera, and managing browser tabs. ",
+    technologies: ["Python"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project2,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "TailwindCSS"],
+  },
+  // {
+  //   title: "Blogging Platform",
+  //   image: project4,
+  //   description:
+  //     "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+  //   technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  // },
+];
+
+export const CONTACT = {
+  // address: "2659 Shadipur Main market West patel nagar New Delhi - 110008",
+  phoneNo: "+91 8860542549 ",
+  email: "dhruv81jangra@gmail.com",
+};
